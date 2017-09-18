15798e6386b2a50bde4a8d331e0285eefa0889cc
konradrog <konrad1210@gmail.com>
2017-09-18 14:43:37 +0200

    add creating new game with question and answers

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,10 +3,18 @@ class Game < ApplicationRecord
 
   def self.create_questions(game_id)
     @game = Game.find(game_id)
-    10.times { @game.questions.create!(game_id: game_id) }
-    @questions = @game.questions
-    @questions.each do |q|
-      q.create_answers(q.id)
+
+    @cars = Car.order("Random()").limit(3).to_a
+
+    3.times { @game.questions.create! }
+
+    @game.questions.each_with_index do |question, i|
+      @correct_answer = Answer.create!(question_id: question.id, car: @cars[i], correct: true)
+
+      other_cars = Car.where(body_style: @cars[i].body_style).where.not(id: @cars[i].id).order("Random()").limit(3).to_a
+      other_cars.each do |other_car|
+        Answer.create!(question_id: question.id, car: other_car)
+      end
     end
   end
 end
