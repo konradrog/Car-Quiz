5ec05e02e17aaa7619eebd012cae7bdf607d9191
konradrog <konrad1210@gmail.com>
2017-09-18 10:27:59 +0200

    add Question model with associations

---
+++ db/migrate/20170918082156_create_questions.rb
@@ -0,0 +1,8 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.references :game, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918081608) do
+ActiveRecord::Schema.define(version: 20170918082156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,12 @@ ActiveRecord::Schema.define(version: 20170918081608) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.bigint "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_questions_on_game_id"
+  end
+
+  add_foreign_key "questions", "games"
 end
