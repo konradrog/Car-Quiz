163faa156bf7fe0b19ba15b743d56a85ddbd84ea
konradrog <konrad1210@gmail.com>
2017-09-18 10:48:58 +0200

    Add all functions to Car mode: new,update,delete with navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :games
+  resources :games do
+    resources :question
+  end
+
+  resources :cars
+
 
   root "games#index"
 end
