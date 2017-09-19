6b605e7b3a8eed668a1f7b5ecc41d4f361530b0b
konradrog <konrad1210@gmail.com>
2017-09-19 19:54:13 +0200

    add rembering last answers on games view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,11 +22,35 @@ class GamesController < ApplicationController
     @questions = @game.questions
   end
 
+  def check_answers
+    good = 0
+    bad = 0
+    wrong_answer = []
+    @game = Game.find(params[:id])
+    @game.questions.each_with_index do |question, i|
+      answer = Answer.find(params[:answers]["#{question.id}"])
+      if answer.correct == true
+        good += 1
+      else
+        bad += 1
+        wrong_answer << i + 1
+      end
+
+    end
+    flash[:notice] = "good answers : #{good}, bad answers #{bad} for questions #{wrong_answer}"
+    unless bad == 0
+      redirect_to game_path(@game, params[:answers].permit!)
+    else
+      redirect_to games_path
+    end
+  end
 
 
   private
 
+
+
   def game_params
-    params.require(:game).permit(:level)
+    params.require(:game).permit(:level, :answers)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :cars
 
+  post "games/:id", to: "games#check_answers"
 
   root "games#index"
 end
