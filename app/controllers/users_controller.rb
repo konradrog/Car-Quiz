5870af7bbbf75c8addb73555b8b076319e489a83
konradrog <konrad1210@gmail.com>
2017-09-26 14:05:53 +0200

    add pagination to user games and % finished games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @games = @user.games.order(id: :desc).limit(10)
+    @games = @user.games.order(id: :desc).paginate(page: params[:page], per_page: 10)
+    finish = @user.games.where(finished: :true).count
+    not_finish = @user.games.where(finished: :false).count
+    @finished = ( finish.to_f / not_finish ) * 100
   end
 end
