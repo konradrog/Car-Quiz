c49ef6f4dd804b5185314d30b72d5165b238ff8d
konradrog <konrad1210@gmail.com>
2017-09-26 14:36:35 +0200

    finally correct version of percent view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @games = @user.games.order(id: :desc).paginate(page: params[:page], per_page: 10)
     finish = @user.games.where(finished: :true).count
-    not_finish = @user.games.where(finished: :false).count
-    if not_finish != 0
-      @finished = ( not_finish / finish.to_f ) * 100
-    else
-      @finished = 100
-    end
+    all = @user.games.count
+    @finished = ( finish.to_f / all ) * 100
   end
 end
