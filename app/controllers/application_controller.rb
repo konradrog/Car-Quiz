3b23fe6c883266bbfb79a24008f4aed85e451178
konradrog <konrad1210@gmail.com>
2017-09-29 09:48:59 +0200

    remove turoblinks to make redirection to login page before login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   end
+  #   redirect_to games_path, :notice => 'You need to log in or sign up'
+  # end
 end
