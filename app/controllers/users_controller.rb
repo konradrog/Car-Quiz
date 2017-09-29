df1ae457a24865e0a72e65454a94801986a14ff6
konradrog <konrad1210@gmail.com>
2017-09-29 11:42:36 +0200

    refactoring sorting in user view:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    
-    if params[:order] == "older_first" || nil
-      @games = @user.games.order(created_at: :asc).paginate(page: params[:page], per_page: 10)
-    elsif params[:order] == "lower_level_first"
-      @games = @user.games.order(level: :asc).paginate(page: params[:page], per_page: 10)
-    elsif params[:order] == "higher_level_first"
-      @games = @user.games.order(level: :desc).paginate(page: params[:page], per_page: 10)
-    elsif params[:order] == "finished_first"
-      @games = @user.games.order(finished: :desc).paginate(page: params[:page], per_page: 10)
-    elsif params[:order] == "unfinished_first"
-      @games = @user.games.order(finished: :asc).paginate(page: params[:page], per_page: 10)
-    else
-      @games = @user.games.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-    end
-
-
+    @games = @user.games
+    choose_order(params)
     finish = @user.games.where(finished: :true).count
-    all = @user.games.count
-    @finished = ( finish.to_f / all ) * 100
+    @finished = ( finish.to_f / @user.games.count ) * 100
+  end
+
+  def choose_order(params)
+    case params[:order]
+    when "older_first" || nil
+      @games = @games.order(created_at: :asc)
+    when "lower_level_first"
+      @games = @games.order(level: :asc)
+    when "higher_level_first"
+      @games = @games.order(level: :desc)
+    when "finished_first"
+      @games = @games.order(finished: :desc)
+    when "unfinished_first"
+      @games = @games.order(finished: :asc)
+    else
+      @games.order(created_at: :desc)
+    end
+    @games = @games.paginate(page: params[:page], per_page: 10)
   end
 end
